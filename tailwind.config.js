fbf3c714999754e7d2841547c7d9b6f13c3832f1
Re9iNee <attarzadeh76@gmail.com>
2022-12-14 14:07:19 +0330

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: "jit",
+    // remove un-used styles in production
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}",
+    ],
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
@@ -8,4 +14,5 @@ module.exports = {
         extend: {},
     },
     plugins: [],
+    darkMode: false,
 };
