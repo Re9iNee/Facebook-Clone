1ecddbfba611b606ad8777f545b59ec6675f5502
Re9iNee <attarzadeh76@gmail.com>
2022-12-18 01:45:48 +0330

    modifying tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: "jit",
-    // remove un-used styles in production
-    purge: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,jsx,ts,tsx}",
-    ],
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
@@ -14,5 +9,5 @@ module.exports = {
         extend: {},
     },
     plugins: [],
-    darkMode: false,
+    media: false,
 };
